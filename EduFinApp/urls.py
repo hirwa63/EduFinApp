7d868532a73523f25b8238b1ce7165ef3ebbfa91
Muteteripeace <pmuteteri0@gmail.com>
2026-03-11 14:01:44 +0200

    Complete Step 9

--- EduFinApp/urls.py
+++ EduFinApp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import testing_view, health_check 
+from core.views import testing_view, health_check, testing_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing', testing_view, name='testing'),
-    path('health', health_check, name='health'),    
+    path('testing/<int:id>', testing_detail_view, name='testing-detail'),
+    path('health', health_check, name='health'),
 ]
